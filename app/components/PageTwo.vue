5cdcf7290bb82b0a5b3c442d4b53186851adbfeb
HETIC <rami.cakir01@gmail.com>
2019-08-16 18:57:05 +0200

    add router link from page1 to page 2

--- app/components/PageTwo.vue
+++ app/components/PageTwo.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="table">
-    <table v-if="datas">
+  <div>
+    <table class="table" v-if="datas">
       <tr style="margin-bottom:30px">
-        <th >date</th>
-        <th >contenu</th>
+        <th>date</th>
+        <th>contenu</th>
       </tr>
       <tr v-for="data in datas">
         <td>{{data.month + '/' + data.day}}</td>
         <td>{{data.content}}</td>
       </tr>
-    
     </table>
 
+    <div v-else class="error">
+      <h1>Veuillez entrer au moins une date</h1>
+      <router-link class="error__link" :to="{ name: 'home' }">Page 1</router-link>
+    </div>
+
+
   </div>
+
 </template>
 
 <script>
